ca1991310ca81d92f2a67e8097a6705b1b43b5fc
Indygos <cyberindygos@gmail.com>
2022-05-12 22:49:26 +0200

    Added split - TBC

--- Tests/FromWork.cs
+++ Tests/FromWork.cs
@@ -22,6 +22,7 @@ namespace FromWork
         [AllureOwner("Sergiusz")]
         [AllureIssue("0001")]
         [AllureName("FromWorkByFootGoogleChrome")]
+        [AllureTag("From work", "Walk", "Chrome")]
         public void GoogleChrome()
         {
             using (IWebDriver driver = new ChromeDriver())
@@ -53,15 +54,21 @@ namespace FromWork
                 byFoot.Click();
                 DemoHelper.Pause();
 
-                IWebElement walkDistance = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.ivN21e.tUEI8e.fontBodyMedium"));
-                string WalkDistance = walkDistance.GetAttribute("double");
-                var WalkDistanceByFoot = Convert.ToDouble(WalkDistance);
-                Assert.True(WalkDistanceByFoot < 1, "The distance is smaller than the 3 km");
+                var walkDistance = "#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.ivN21e.tUEI8e.fontBodyMedium";
+                //var Distance = "#section-directions-trip-0 div[jsan]:nth-child(2)";
+                var distance = driver.FindElement(By.CssSelector(walkDistance)).Text;
+                distance = distance.Split()[0];
+                //string WalkDistance = walkDistance.GetAttribute("double");
+                var WalkDistanceByFoot = Convert.ToDouble(distance);
+                Assert.True(WalkDistanceByFoot < 3.0);
 
-                IWebElement walkTime = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.Fk3sm.fontHeadlineSmall"));
-                string WalkTime = walkDistance.GetAttribute("double");
-                var WalkTimeByFoot = Convert.ToDouble(WalkTime);
-                Assert.True(WalkDistanceByFoot < 10, "The walk time is smaller than the 40 min");
+                var walkTime = "#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.Fk3sm.fontHeadlineSmall";
+                //string WalkTime = walkDistance.GetAttribute("double");
+                //var WalkTimeByFoot = Convert.ToDouble(distance);
+                var time = driver.FindElement(By.CssSelector(walkTime)).Text;
+                time = time.Split()[0];
+                var WalkTimeByFoot = Convert.ToDouble(time);
+                Assert.True(WalkTimeByFoot < 40.0);
 
                 ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
                 Screenshot screenshot = takesScreenshot.GetScreenshot();
@@ -69,7 +76,12 @@ namespace FromWork
             }
         }
 
-        [Test]
+        [Test(Description = "Going from work by foot - Mozilla Firefox")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("Sergiusz")]
+        [AllureIssue("0002")]
+        [AllureName("FromWorkByFootMozillaFirefox")]
+        [AllureTag("From work", "Walk", "Firefox")]
         public void MozillaFirefox()
         {
             using (IWebDriver driver = new FirefoxDriver())
@@ -101,19 +113,131 @@ namespace FromWork
                 byFoot.Click();
                 DemoHelper.Pause();
 
-                IWebElement walkDistance = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)"));
-                string WalkDistance = walkDistance.GetAttribute("int");
-                var WalkDistanceByFoot = Convert.ToDouble(WalkDistance);
-                Assert.True(WalkDistanceByFoot < 3, "The distance is smaller than the 3 km");
+                var walkDistance = "#section-directions-trip-0 > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)";
+                //var Distance = "#section-directions-trip-0 div[jsan]:nth-child(2)";
+                var distance = driver.FindElement(By.CssSelector(walkDistance)).Text;
+                distance = distance.Split()[0];
+                //string WalkDistance = walkDistance.GetAttribute("double");
+                var WalkDistanceByFoot = Convert.ToDouble(distance);
+                Assert.True(WalkDistanceByFoot < 3);
 
-                IWebElement walkTime = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)"));
-                string WalkTime = walkDistance.GetAttribute("int");
-                var WalkTimeByFoot = Convert.ToDouble(WalkTime);
-                Assert.True(WalkDistanceByFoot < 15, "The walk time is smaller than the 40 min");
+                var walkTime = "#section-directions-trip-0 > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)";
+                //string WalkTime = walkDistance.GetAttribute("double");
+                //var WalkTimeByFoot = Convert.ToDouble(distance);
+                var time = driver.FindElement(By.CssSelector(walkTime)).Text;
+                time = time.Split()[0];
+                var WalkTimeByFoot = Convert.ToDouble(time);
+                Assert.True(WalkTimeByFoot < 40);
 
                 ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
                 Screenshot screenshot = takesScreenshot.GetScreenshot();
                 screenshot.SaveAsFile("ByFootFromWorkFirefox.jpg", ScreenshotImageFormat.Jpeg);
+            }
+        }
+
+        [Test(Description = "Going from work by bike - Google Chrome")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("Sergiusz")]
+        [AllureIssue("0003")]
+        [AllureName("FromWorkByBikeGoogleChrome")]
+        [AllureTag("From work", "Bike", "Chrome")]
+        public void GoogleChrome2()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(MapsUrl);
+                DemoHelper.Pause();
+
+                IWebElement searchInput = driver.FindElement(By.Id("searchboxinput"));
+                searchInput.SendKeys("Plac Defilad 1");
+                DemoHelper.Pause();
+
+                IWebElement searchButton = driver.FindElement(By.Id("searchbox-searchbutton"));
+                searchButton.Click();
+                DemoHelper.Pause();
+
+                IWebElement setCourse = driver.FindElement(By.ClassName("EgL07d"));
+                setCourse.Click();
+                DemoHelper.Pause();
+
+                IWebElement fromWhereSearchInput = driver.FindElement(By.CssSelector("#sb_ifc51 > input"));
+                fromWhereSearchInput.SendKeys("Chłodna 51");
+                DemoHelper.Pause();
+
+                IWebElement searchForRoad = driver.FindElement(By.CssSelector("#directions-searchbox-0 > button.mL3xi"));
+                searchForRoad.Click();
+                DemoHelper.Pause();
+
+                IWebElement byBike = driver.FindElement(By.CssSelector("#omnibox-directions > div > div:nth-child(2) > div > div > div.IqV6ub > div.FkdJRd > div.oya4hc.vxq1Hc > button > img"));
+                byBike.Click();
+                DemoHelper.Pause();
+
+                IWebElement rideDistance = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.ivN21e.tUEI8e.fontBodyMedium"));
+                string RideDistance = rideDistance.GetAttribute("int");
+                var RideDistanceByBike = Convert.ToDouble(RideDistance);
+                Assert.True(RideDistanceByBike < 3);
+
+                IWebElement rideTime = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.Fk3sm.fontHeadlineSmall"));
+                string RideTime = rideTime.GetAttribute("int");
+                var RideTimeByBike = Convert.ToDouble(RideTime);
+                Assert.True(RideTimeByBike < 40);
+
+                ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
+                Screenshot screenshot = takesScreenshot.GetScreenshot();
+                screenshot.SaveAsFile("FromWorkByBikeChrome.jpg", ScreenshotImageFormat.Jpeg);
+            }
+        }
+
+        [Test(Description = "Going from work by bike - Mozilla Firefox")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("Sergiusz")]
+        [AllureIssue("0004")]
+        [AllureName("FromWorkByBikeMozillaFirefox")]
+        [AllureTag("From work", "Bike", "Firefox")]
+        public void MozillaFirefox2()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl(MapsUrl);
+                DemoHelper.Pause();
+
+                IWebElement searchInput = driver.FindElement(By.Id("searchboxinput"));
+                searchInput.SendKeys("Plac Defilad 1");
+                DemoHelper.Pause();
+
+                IWebElement searchButton = driver.FindElement(By.Id("searchbox-searchbutton"));
+                searchButton.Click();
+                DemoHelper.Pause();
+
+                IWebElement setCourse = driver.FindElement(By.CssSelector("div.etWJQ:nth-child(1) > button:nth-child(1) > span:nth-child(1) > img:nth-child(1)"));
+                setCourse.Click();
+                DemoHelper.Pause();
+
+                IWebElement fromWhereSearchInput = driver.FindElement(By.CssSelector("#sb_ifc51 > input:nth-child(1)"));
+                fromWhereSearchInput.SendKeys("Chłodna 51");
+                DemoHelper.Pause();
+
+                IWebElement searchForRoad = driver.FindElement(By.CssSelector("#directions-searchbox-0 > button:nth-child(2)"));
+                searchForRoad.Click();
+                DemoHelper.Pause();
+
+                IWebElement byBike = driver.FindElement(By.CssSelector(".IqV6ub > div:nth-child(1) > div:nth-child(1) > button:nth-child(1) > img:nth-child(1)"));
+                byBike.Click();
+                DemoHelper.Pause();
+
+                IWebElement rideDistance = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.ivN21e.tUEI8e.fontBodyMedium"));
+                string RideDistance = rideDistance.GetAttribute("int");
+                var RideDistanceByBike = Convert.ToDouble(RideDistance);
+                Assert.True(RideDistanceByBike < 3, "The distance is smaller than the 3 km");
+
+                IWebElement rideTime = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)"));
+                string RideTime = rideTime.GetAttribute("int");
+                var RideTimeByBike = Convert.ToDouble(RideTime);
+                Assert.True(RideTimeByBike < 15, "The walk time is smaller than the 40 min");
+
+                ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
+                Screenshot screenshot = takesScreenshot.GetScreenshot();
+                screenshot.SaveAsFile("FromWorkByBikeFirefox.jpg", ScreenshotImageFormat.Jpeg);
             }
         }
     }
@@ -122,7 +246,12 @@ namespace FromWork
     {
         private const string MapsUrl = "https://www.google.pl/maps";
 
-        [Test]
+        [Test(Description = "Going from work by bike - Google Chrome")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("Sergiusz")]
+        [AllureIssue("0003")]
+        [AllureName("FromWorkByBikeGoogleChrome")]
+        [AllureTag("From work", "Bike", "Chrome")]
         public void GoogleChrome()
         {
             using (IWebDriver driver = new ChromeDriver())
@@ -170,7 +299,12 @@ namespace FromWork
             }
         }
 
-        [Test]
+        [Test(Description = "Going from work by bike - Mozilla Firefox")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("Sergiusz")]
+        [AllureIssue("0004")]
+        [AllureName("FromWorkByBikeMozillaFirefox")]
+        [AllureTag("From work","Bike", "Firefox")]
         public void MozillaFirefox()
         {
             using (IWebDriver driver = new FirefoxDriver())
